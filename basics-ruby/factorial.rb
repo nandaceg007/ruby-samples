ccadc35f13ffd1232747f563cfa532888f1d02ef
Jayasenthil <jayasenthilceg@gmail.com>
2014-07-10 12:01:35 +0530

    Basic factorial program in ruby

---
+++ basics-ruby/factorial.rb
@@ -0,0 +1,13 @@
+begin
+def fact(n)
+ return 1 if n == 1
+ return n * fact(n-1)
+end
+puts "Enter the number:"
+n = gets.to_i
+puts "The factorial is:"
+puts fact(n)
+
+rescue => error
+puts error
+end 
