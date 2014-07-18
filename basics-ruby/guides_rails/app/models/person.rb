f97f2771a2fff5690f1af21501477aa9b520884a
Jayasenthil <jayasenthilceg@gmail.com>
2014-07-18 17:09:49 +0530

    nested form create,update and delete options implement

--- basics-ruby/guides_rails/app/models/person.rb
+++ basics-ruby/guides_rails/app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
-  has_many :addresses
-  accepts_nested_attributes_for :addresses
+  has_many :addresses, :dependent => :destroy
+  accepts_nested_attributes_for :addresses, :allow_destroy => true
 end
